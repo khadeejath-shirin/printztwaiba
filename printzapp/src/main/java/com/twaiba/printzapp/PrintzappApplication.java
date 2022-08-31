d15942839424c4c9a6ab92b3b5cc85eea7755cf3
khadeejath_shirin <khadeejathshirin871@gmail.com>
2022-08-31 18:41:20 +0400

    initial commit

---
+++ printzapp/src/main/java/com/twaiba/printzapp/PrintzappApplication.java
@@ -0,0 +1,13 @@
+package com.twaiba.printzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintzappApplication.class, args);
+	}
+
+}
---
+++ printzapp/src/test/java/com/twaiba/printzapp/PrintzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twaiba.printzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
